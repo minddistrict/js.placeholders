29ff3acb1c18f3c5635f409c77ade97ce07df305
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2014-10-22 09:09:03 +0200

    add conditional renderer for ie9 and lower

--- js/placeholders/__init__.py
+++ js/placeholders/__init__.py
@@ -1,6 +1,13 @@
 from fanstatic import Library, Resource
+from fanstatic.core import render_js
+
+
+def ie_lte9_conditional_inclusion(url):
+    return '''<!--[if lte IE 9]>{}<![endif]-->'''.format(render_js(url))
+
 
 library = Library('placeholders.js', 'resources')
 
 placeholders = Resource(
-    library, 'placeholders.js', minified='placeholders.min.js')
+    library, 'placeholders.js', minified='placeholders.min.js',
+    renderer=ie_lte9_conditional_inclusion)
